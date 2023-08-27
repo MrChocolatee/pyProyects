f0556ca68b68b9add8071061acf6ca7191191801
MrChocolate <mrchocolatepr@gmail.com>
2023-08-27 18:05:36 -0400

    agregando proyecto Instabot

---
+++ instaBot/replicantDreamsInstaBot.py
@@ -0,0 +1,8 @@
+from instabot import Bot
+bot = Bot()
+
+#Iniciamos sesión
+bot.login(username="the_mrchocolate", password="Megu221092*")
+# Hacemos un post agregando dos parámetros: el archivo y el caption
+# bot.upload_photo("ChavezXi.jpg", caption="Chavez Jingping")
+# Subimos una Story
